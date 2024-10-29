e9027a54880d316c6c7c297d0a868d5c429a9421
hatdogdestroyer <tristanzymon@gmail.com>
2024-10-29 13:49:15 +0400

    Add files via upload

--- Ex10.py
+++ Ex10.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 22 13:20:03 2024
+# Exercise 10: Is it even
 
-@author: trist
-"""
+
+# This is how i wrote the function of the code
+def main():
+# This is just a prompt the user input a number
+    odd_or_even=int(input("Input a number: "))
+# This runs the code if a number is odd or even    
+    if odd_or_even % 2 == 0:
+        result = "The number is even."
+        print("even")
+    else:
+        result = "The number is odd."
+        print (result)
+# The if and else helps the user identify if the code is odd or even
+main()
+# Now you run the code
+    
+        
+        
+        
+
+
+
 
